331487fdf67c4054d2c5b7f271248a17f5f0ea1f
balaji <abalaji1672003@gmail.com>
2024-08-16 14:22:41 +0530

    git check

--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // Define your routes here
 router.get('/', (req, res) => {
   res.send('API Route is working');
+  console.log('router ser successfully');
 });
 
 // Export the router
