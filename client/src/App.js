f2cb135dbe1528f4dd35335c4a628e6f3491649f
balaji <abalaji1672003@gmail.com>
2024-08-17 21:36:45 +0530

    hosting backend in render

--- client/src/App.js
+++ client/src/App.js
@@ -10,26 +10,26 @@ function App() {
    },[])
   const fetchtodos=async()=>
   {
-    const response=await axios.get('http://localhost:4000/api/todos');
+    const response=await axios.get('https://todo-react-app-himf.onrender.com/api/todos');
     setTodos(response.data);
   }
   const addTodo=async()=>
   {
     if(!todo)
       return alert('please enter the task');
-    const response=await axios.post('http://localhost:4000/api/todos',{title: todo});
+    const response=await axios.post('https://todo-react-app-himf.onrender.com/api/todos',{title: todo});
     setTodos([...todos,response.data]);
     setnewTodo("");
   }
   const updateTodo =async(id)=>
   {
     const todo=todos.find((todo)=>id===todo._id)
-     const response=await axios.put(`http://localhost:4000/api/todos/${id}`,{completed: !todo.completed});
+     const response=await axios.put(`https://todo-react-app-himf.onrender.com/api/todos/${id}`,{completed: !todo.completed});
      setTodos(todos.map((todo)=>(todo._id===id?response.data:todo)))
   }
   const deleteTodo =async(id)=>
   {
-    await axios.delete(`http://localhost:4000/api/todos/${id}`);
+    await axios.delete(`https://todo-react-app-himf.onrender.com/api/todos/${id}`);
     setTodos(todos.filter((todo)=>(id!==todo._id)));
   }
   return(
